c93b38667ffc41736bad22082a14eb95c6314409
Kwame Owusu <alfred.owusu@zing.dev>
2025-09-09 20:36:48 +0100

    chaged binaries

--- Formula/sidl.rb
+++ Formula/sidl.rb
@@ -7,17 +7,17 @@ class Sidl < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kwame-Owusu/sidl/releases/download/v1.0.0/sidl-darwin-arm64.tar.gz"
-      sha256 "d7dffed21e6b3d737ea16bd86a6b74d54af8e99c2c34f5e7e4e7102c7705bb91"
+      sha256 "772d465e4a7be860c3f0c44e0905af974e8c8a21e1509a87924e305caeaaccd1"
     else
       url "https://github.com/kwame-Owusu/sidl/releases/download/v1.0.0/sidl-darwin-amd64.tar.gz"
-      sha256 "320b71e8521f2f82a6ed5f5837c4217cf7a1408d3a6c0965991f272261133adc"
+      sha256 "bceba392f6581c93105b15b8416ccb2e4eb9ece7c23bb3e6b924305319818f40"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/kwame-Owusu/sidl/releases/download/v1.0.0/sidl-linux-amd64.tar.gz"
-      sha256 "30f0179405abf9c433837fc043ac67f1485494513d31b7946be3ec091095eb28"
+      sha256 "6fe5465016b576726e9e7409dd48e14df20ff9c1682a4139e57b8f1c2e69f17f"
     end
   end
 
